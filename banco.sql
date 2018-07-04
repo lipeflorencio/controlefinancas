b9f928c73fb3394fe46583a1605df0cf39a13e6c
lipeflorencio <38772521+lipeflorencio@users.noreply.github.com>
2018-07-04 01:31:52 -0300

    Revert "Merge branch 'master' of https://github.com/lipeflorencio/controlefinancas"
    This reverts commit 69a17c96aabef1e19cb54e555f406d030c82628e, reversing
    changes made to 0069c4eca5508101ae22a140e362cddd9094f63a.

--- banco.sql
+++ banco.sql
@@ -2,12 +2,10 @@ drop database if exists financeiro;
 
 create database financeiro;
 
-use financeiro;
-
 create table categoria(
     id integer auto_increment primary key,
     tipo varchar(20) not null,
-    nome varchar(40) not null
+    descricao varchar(40) not null
 );
 
 desc categoria;
